12908baddce6d5c4b0468f558b52025c6794436d
Misael Madrigal <misaelmt@gmail.com>
2015-11-13 17:35:59 -0500

    Testing nil parameters

--- main.lua
+++ main.lua
@@ -1,9 +1,12 @@
 local loggly = require 'LogglyBufferMod'
 
 
-loggly:init('861d2eb8-add3-4e66-ae02-fe147fa7e5aa', 2)
+loggly:init('861d2eb8-add3-4e66-ae02-fe147fa7e5aa')
+loggly.debug = false
 
-
-loggly:log({msg = [[11 Hola
+loggly:log({msg = [[12 Hola
 mundo]]})
-loggly:log('11 Hola mundo sin salto')
+loggly:log('12 Hola mundo sin salto')
+loggly:log()
+loggly:log('tres')
+loggly:log(4)
